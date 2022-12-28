a3b15f4c59f4bd95c17d5bf6fcff279824d7da95
hedgehog <pm22hedgehog@gmail.com>
2022-12-28 20:37:34 +0900

    update

--- script/customMenu.js
+++ script/customMenu.js
@@ -7,14 +7,14 @@
  * @see addMenu::createIndex()
  * @OnlyCurrentDoc 
  */
- function onOpen(event) {
-  let menuItems = [
-    { name: '目次作成', functionName: 'createIndex' },
-    { name: '行サイズを調整する', functionName : 'adjustRowSize'},
-    { name: '二分法による下限第3種加算補正値探索', functionName : 'b3BinarySearch'}
-  ];
-  let sheet = SpreadsheetApp.getActiveSpreadsheet();
-  sheet.addMenu('スクリプト', menuItems);
+function onOpen(event) {
+    let menuItems = [
+        { name: '目次作成', functionName: 'createIndex' },
+        { name: '行サイズを調整する', functionName: 'adjustRowSize' },
+        { name: '二分法による下限第3種加算補正値探索', functionName: 'b3BinarySearch' }
+    ];
+    let sheet = SpreadsheetApp.getActiveSpreadsheet();
+    sheet.addMenu('スクリプト', menuItems);
 }
 
 /**
@@ -26,33 +26,33 @@
  */
 function createIndex() {
 
-  // アクティブなスプレッドシートを取得する
-  let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    // アクティブなスプレッドシートを取得する
+    let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
-  // アクティブなシートを取得する
-  let sheet = spreadsheet.getActiveSheet();
+    // アクティブなシートを取得する
+    let sheet = spreadsheet.getActiveSheet();
 
-  // アクティブな範囲を取得する
-  let range = sheet.getActiveRange();
+    // アクティブな範囲を取得する
+    let range = sheet.getActiveRange();
 
-  // すべてのシートを取得する
-  let sheets = spreadsheet.getSheets();
+    // すべてのシートを取得する
+    let sheets = spreadsheet.getSheets();
 
-  // すべてのシートの名前を取得してnameListに格納する
-  let nameList = sheets.map(sheet => sheet.getName());
+    // すべてのシートの名前を取得してnameListに格納する
+    let nameList = sheets.map(sheet => sheet.getName());
 
-  // 取得したシートのgidを格納する
-  let gidList = sheets.map(sheet => sheet.getSheetId());
+    // 取得したシートのgidを格納する
+    let gidList = sheets.map(sheet => sheet.getSheetId());
 
-  // すべてのシートについて
-  for (let i = 0; i < sheets.length; i++) {
+    // すべてのシートについて
+    for (let i = 0; i < sheets.length; i++) {
 
-    // 組み込み関数を作って格納する
-    let formulaStr = `=hyperlink(\n   "#gid=${gidList[i]}",\n   "${nameList[i]}"\n )`;
+        // 組み込み関数を作って格納する
+        let formulaStr = `=hyperlink(\n   "#gid=${gidList[i]}",\n   "${nameList[i]}"\n )`;
 
-    // 出力する
-    sheet.getRange(range.getRowIndex() + i, range.getColumn()).setFormula(formulaStr);
-  }
+        // 出力する
+        sheet.getRange(range.getRowIndex() + i, range.getColumn()).setFormula(formulaStr);
+    }
 }
 
 /**
@@ -62,53 +62,53 @@ function createIndex() {
  */
 function adjustRowSize() {
 
-  // 調整したいシートの名前
-  const adjustTargetSheets = ['input', 'calc', 'invCalc', 'report'];
+    // 調整したいシートの名前
+    const adjustTargetSheets = ['input', 'calc', 'invCalc', 'report'];
 
-  const MIN = 3;
-  const MAX = 10000;
+    const MIN = 3;
+    const MAX = 10000;
 
-  // メッセージボックスの作成と表示, 入力受付
-  const message = `シート${adjustTargetSheets.join(', ')}の行サイズを拡張または縮小します。\\n目標の行サイズを半角の自然数で入力してください。\\n${MIN}以下または${MAX}以上の場合は何もしません。`;
-  const targetRowSize = Number(Browser.inputBox(message));
-  
-  // 例外処理
-  if (targetRowSize <= MIN || targetRowSize >= MAX) return;
+    // メッセージボックスの作成と表示, 入力受付
+    const message = `シート${adjustTargetSheets.join(', ')}の行サイズを拡張または縮小します。\\n目標の行サイズを半角の自然数で入力してください。\\n${MIN}以下または${MAX}以上の場合は何もしません。`;
+    const targetRowSize = Number(Browser.inputBox(message));
 
-  // アクティブなスプレッドシートを取得する
-  let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    // 例外処理
+    if (targetRowSize <= MIN || targetRowSize >= MAX) return;
 
-  // すべてのシートを取得する
-  let sheets = spreadsheet.getSheets();
+    // アクティブなスプレッドシートを取得する
+    let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
-  // すべてのシートの名前を取得する
-  let nameList = sheets.map(sheet => sheet.getName());
+    // すべてのシートを取得する
+    let sheets = spreadsheet.getSheets();
 
-  // シートを1つずつ走査して「調整したいシートの名前」のみの行サイズを変更する
-  for (let i = 0; i < nameList.length; i++) {
+    // すべてのシートの名前を取得する
+    let nameList = sheets.map(sheet => sheet.getName());
 
-    // 走査中のシートが「調整したいシートの名前」であるか
-    if (adjustTargetSheets.includes(nameList[i])) {
-      
-      // 調整したいシートの行サイズ
-      const currentRowSize = sheets[i].getMaxRows();
+    // シートを1つずつ走査して「調整したいシートの名前」のみの行サイズを変更する
+    for (let i = 0; i < nameList.length; i++) {
 
-      // 行サイズの差分
-      const diffRowSize = targetRowSize - currentRowSize;
+        // 走査中のシートが「調整したいシートの名前」であるか
+        if (adjustTargetSheets.includes(nameList[i])) {
 
-      // 差分に応じて処理
-      if (diffRowSize > 0) {
-        // 最終行の後に、足りない行を追加する
-        sheets[i].insertRowsAfter(currentRowSize, diffRowSize);
-      } else if (diffRowSize < 0) {
-        // 目標の行より下の行を削除する
-        sheets[i].deleteRows(targetRowSize + 1, -diffRowSize);
-      }
+            // 調整したいシートの行サイズ
+            const currentRowSize = sheets[i].getMaxRows();
 
-      // diffRowSize == 0の場合は何もしなくてよい
+            // 行サイズの差分
+            const diffRowSize = targetRowSize - currentRowSize;
 
+            // 差分に応じて処理
+            if (diffRowSize > 0) {
+                // 最終行の後に、足りない行を追加する
+                sheets[i].insertRowsAfter(currentRowSize, diffRowSize);
+            } else if (diffRowSize < 0) {
+                // 目標の行より下の行を削除する
+                sheets[i].deleteRows(targetRowSize + 1, -diffRowSize);
+            }
+
+            // diffRowSize == 0の場合は何もしなくてよい
+
+        }
     }
-  }
 }
 
 /**
@@ -117,40 +117,40 @@ function adjustRowSize() {
  * @OnlyCurrentDoc 
  */
 function b3BinarySearch() {
-  let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  let console = spreadsheet.getSheetByName('console');
+    let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    let console = spreadsheet.getSheetByName('console');
 
-  //function　getB3() { return Number(console.getRange('P3').getValue()); }
-  function getB3_inf() { return Number(console.getRange('P25').getValue()); }
-  function getB3_max() { return Number(console.getRange('R25').getValue()); }
-  function isUnexpectedDamage() { return Number(console.getRange('P36').getValue()) + Number(console.getRange('R36').getValue());}
+    //function　getB3() { return Number(console.getRange('P3').getValue()); }
+    function getB3_inf() { return Number(console.getRange('P25').getValue()); }
+    function getB3_max() { return Number(console.getRange('R25').getValue()); }
+    function isUnexpectedDamage() { return Number(console.getRange('P36').getValue()) + Number(console.getRange('R36').getValue()); }
 
-  function setB3(num) { Number(console.getRange('P3').setValue(num)); }
-  //function　setB3_inf(num) { Number(console.getRange('P3').setValue(num)); }
-  //function　setB3_max(num) { Number(console.getRange('P3').setValue(num)); }
+    function setB3(num) { Number(console.getRange('P3').setValue(num)); }
+    //function　setB3_inf(num) { Number(console.getRange('P3').setValue(num)); }
+    //function　setB3_max(num) { Number(console.getRange('P3').setValue(num)); }
 
-  let b3_inf = getB3_inf();
-  let b3_max = getB3_max();
-  let mid = (b3_inf + b3_max) / 2;
-  const accuracy = 1e-10;
+    let b3_inf = getB3_inf();
+    let b3_max = getB3_max();
+    let mid = (b3_inf + b3_max) / 2;
+    const accuracy = 1e-10;
 
-  while (1) {
-    let temp_acc = mid;
-    let temp_is;
+    while (1) {
+        let temp_acc = mid;
+        let temp_is;
 
-    setB3(mid);
+        setB3(mid);
 
-    if (temp_is = isUnexpectedDamage()) {
-      b3_inf = mid;
-      mid = (mid + b3_max) / 2;
-    } else {
-      b3_max = mid;
-      mid = (b3_inf + mid) / 2;
+        if (temp_is = isUnexpectedDamage()) {
+            b3_inf = mid;
+            mid = (mid + b3_max) / 2;
+        } else {
+            b3_max = mid;
+            mid = (b3_inf + mid) / 2;
+        }
+
+        Logger.log(mid + ', ' + temp_is)
+
+        if (Math.abs(temp_acc - mid) < accuracy && temp_is) break;
+
     }
-
-    Logger.log(mid + ', ' + temp_is)
-
-    if (Math.abs(temp_acc - mid) < accuracy && temp_is) break;
-    
-  }
 }
--- script/tpBonus.js
+++ script/tpBonus.js
@@ -5,60 +5,60 @@
  * @param {Array<number>} gearRf 攻撃艦の装備改修値
  */
 class TpAttacker {
-  constructor(attackerId, gearId, gearRf) {
-    /** @type {number} 攻撃艦の艦船ID */
-    this.id = attackerId;
-    /** @type {number} api_mst_shipでの位置 */
-    this.index = TpAttacker.binarySearch(this.id, api_mst_ship.map(ship => ship.api_id));
-    /** @type {JSON} アクセスのショートカットのため */
-    this.elem = api_mst_ship[this.index];
+    constructor(attackerId, gearId, gearRf) {
+        /** @type {number} 攻撃艦の艦船ID */
+        this.id = attackerId;
+        /** @type {number} api_mst_shipでの位置 */
+        this.index = TpAttacker.binarySearch(this.id, api_mst_ship.map(ship => ship.api_id));
+        /** @type {JSON} アクセスのショートカットのため */
+        this.elem = api_mst_ship[this.index];
 
-    /** @type {string} 攻撃艦の艦名 */
-    this.name = this.elem.api_name;
-    /** @type {string} 攻撃艦のよみ */
-    this.yomi = this.elem.api_yomi;
-    /** @type {string} 攻撃艦の艦種 */
-    this.stype = SHIP_TYPE.find(e => e.stype == this.elem.api_stype).name;
-    /** @type {string} 攻撃艦の艦型 */
-    this.ctype = CLASS_TYPE.find(e => e.ctype == this.elem.api_ctype).name;
-    /** @type {string} 攻撃艦の国籍 main.jsに書いてあるらしい */
-    this.country = CLASS_TYPE.find(e => e.ctype == this.elem.api_ctype).country;
-    /** @type {Array<number>} 攻撃艦の装備ID */
-    this.gearId = [...gearId.filter(id => id > 0)];
-    /** @type {Array<number>} 攻撃艦の装備改修値 */
-    this.gearRf = [...gearRf.filter((rf, i) => this.gearId[i] > 0)];
+        /** @type {string} 攻撃艦の艦名 */
+        this.name = this.elem.api_name;
+        /** @type {string} 攻撃艦のよみ */
+        this.yomi = this.elem.api_yomi;
+        /** @type {string} 攻撃艦の艦種 */
+        this.stype = SHIP_TYPE.find(e => e.stype == this.elem.api_stype).name;
+        /** @type {string} 攻撃艦の艦型 */
+        this.ctype = CLASS_TYPE.find(e => e.ctype == this.elem.api_ctype).name;
+        /** @type {string} 攻撃艦の国籍 main.jsに書いてあるらしい */
+        this.country = CLASS_TYPE.find(e => e.ctype == this.elem.api_ctype).country;
+        /** @type {Array<number>} 攻撃艦の装備ID */
+        this.gearId = [...gearId.filter(id => id > 0)];
+        /** @type {Array<number>} 攻撃艦の装備改修値 */
+        this.gearRf = [...gearRf.filter((rf, i) => this.gearId[i] > 0)];
 
-    /** @type スロットごとの砲撃戦.航空攻撃 雷装装備ボーナス */
-    this.tpBonusPerSlot = new Array(this.gearId.length).fill(Infinity);
-    /** @type 最小砲撃戦.航空攻撃 雷装装備ボーナス */
-    this.tpBonus = Infinity;
-  };
+        /** @type スロットごとの砲撃戦.航空攻撃 雷装装備ボーナス */
+        this.tpBonusPerSlot = new Array(this.gearId.length).fill(Infinity);
+        /** @type 最小砲撃戦.航空攻撃 雷装装備ボーナス */
+        this.tpBonus = Infinity;
+    };
 
-  /**
-   * @method bunarySearch 二分探索して、その位置を返す
-   * @param {number} searchValue 検索値
-   * @param {Array<number>} sortedData 昇順ソート済みの検索対象データ
-   * @return {number} 位置を返す エラーは-1
-   * @see https://qiita.com/may88seiji/items/189002cb497e61578114
-   */
-  static binarySearch(searchValue, sortedData) {
-    let index = -1;
-    let left = 0;
-    let right = sortedData.length - 1;
-    let middle;
-    while (left <= right) {
-      middle = Math.floor((left + right) / 2);
-      if (sortedData[middle] == searchValue) {
-        index = middle;
-        break;
-      } else if (sortedData[middle] < searchValue) {
-        left = middle + 1;
-      } else {
-        right = middle - 1;
-      }
+    /**
+     * @method bunarySearch 二分探索して、その位置を返す
+     * @param {number} searchValue 検索値
+     * @param {Array<number>} sortedData 昇順ソート済みの検索対象データ
+     * @return {number} 位置を返す エラーは-1
+     * @see https://qiita.com/may88seiji/items/189002cb497e61578114
+     */
+    static binarySearch(searchValue, sortedData) {
+        let index = -1;
+        let left = 0;
+        let right = sortedData.length - 1;
+        let middle;
+        while (left <= right) {
+            middle = Math.floor((left + right) / 2);
+            if (sortedData[middle] == searchValue) {
+                index = middle;
+                break;
+            } else if (sortedData[middle] < searchValue) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return index;
     }
-    return index;
-  }
 }
 
 /**
@@ -69,113 +69,113 @@ class TpAttacker {
  */
 function getTpBonus(attacker) {
 
-  // 装備スロット1つずつ
-  for (let i = 0; i < attacker.gearId.length; i++) {
+    // 装備スロット1つずつ
+    for (let i = 0; i < attacker.gearId.length; i++) {
 
-    // 368	https://wikiwiki.jp/kancolle/Swordfish%20Mk.III改(水上機型)
-    if (attacker.gearId[i] == GEAR_ID_MAP['Swordfish Mk.III改(水上機型)']) {
-      if (attacker.name == 'Gotoland andra') {
-        attacker.tpBonusPerSlot[i] = 2;
-        continue;
-      }
+        // 368	https://wikiwiki.jp/kancolle/Swordfish%20Mk.III改(水上機型)
+        if (attacker.gearId[i] == GEAR_ID_MAP['Swordfish Mk.III改(水上機型)']) {
+            if (attacker.name == 'Gotoland andra') {
+                attacker.tpBonusPerSlot[i] = 2;
+                continue;
+            }
+        }
+
+        // 369	https://wikiwiki.jp/kancolle/Swordfish%20Mk.III改(水上機型／熟練)
+        if (attacker.gearId[i] == GEAR_ID_MAP['Swordfish Mk.III改(水上機型/熟練)']) {
+            if (attacker.name == 'Gotoland andra') {
+                attacker.tpBonusPerSlot[i] = 3;
+                continue;
+            }
+        }
+
+        // 372	https://wikiwiki.jp/kancolle/天山一二型甲
+        if (attacker.gearId[i] == GEAR_ID_MAP['天山一二型甲']) {
+            if (attacker.ctype == '祥鳳型' && /改二/.test(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (['翔鶴型', '大鳳型'].includes(attacker.ctype)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (/龍鳳改/.test(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                attacker.tpBonusPerSlot[i] += /改二/.test(attacker.name) ? 1 : 0;
+                continue;
+            }
+        }
+
+        // 373	https://wikiwiki.jp/kancolle/天山一二型甲改(空六号電探改装備機)
+        if (attacker.gearId[i] == GEAR_ID_MAP['天山一二型甲改(空六号電探改装備機)']) {
+            if (attacker.ctype == '祥鳳型' && /改/.test(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (attacker.ctype == '龍鳳型') {
+                attacker.tpBonusPerSlot[i] = 1;
+                attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二' ? 1 : 0;
+                attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二戊' ? 2 : 0;
+                continue;
+            } else if (attacker.ctype == '千歳型' && /航改/.test(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (attacker.ctype == '飛鷹型') {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (['翔鶴型', '大鳳型'].includes(attacker.ctype)) {
+                attacker.tpBonusPerSlot[i] = 2;
+                continue;
+            } else if (['鈴谷航改二', '熊野航改二'].includes(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 2;
+                continue;
+            }
+        }
+
+        // 374	https://wikiwiki.jp/kancolle/天山一二型甲改(熟練／空六号電探改装備機)
+        if (attacker.gearId[i] == GEAR_ID_MAP['天山一二型甲改(熟練/空六号電探改装備機)']) {
+            if (attacker.ctype == '祥鳳型' && /改/.test(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (attacker.ctype == '龍鳳型') {
+                attacker.tpBonusPerSlot[i] = 1;
+                attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二' ? 1 : 0;
+                attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二戊' ? 2 : 0;
+                continue;
+            } else if (attacker.ctype == '千歳型') {
+                attacker.tpBonusPerSlot[i] = 1;
+                continue;
+            } else if (attacker.ctype == '飛鷹型') {
+                attacker.tpBonusPerSlot[i] = 2;
+                continue;
+            } else if (['翔鶴型', '大鳳型'].includes(attacker.ctype)) {
+                attacker.tpBonusPerSlot[i] = 3;
+                continue;
+            } else if (['鈴谷航改二', '熊野航改二'].includes(attacker.name)) {
+                attacker.tpBonusPerSlot[i] = 2;
+                continue;
+            }
+        }
+
+        // 424	https://wikiwiki.jp/kancolle/Barracuda%20Mk.II
+        if (attacker.gearId[i] == GEAR_ID_MAP['Barracuda Mk.II']) {
+            if (attacker.country == 'イギリス' && ['軽空母', '正規空母', '装甲空母'].includes(attacker.stype)) {
+                attacker.tpBonusPerSlot[i] = 3;
+                continue;
+            }
+        }
+
+        // 425	https://wikiwiki.jp/kancolle/Barracuda%20Mk.III
+        if (attacker.gearId[i] == GEAR_ID_MAP['Barracuda Mk.III']) {
+            if (attacker.country == 'イギリス' && ['軽空母', '正規空母', '装甲空母'].includes(attacker.stype)) {
+                attacker.tpBonusPerSlot[i] = 1;
+                attacker.tpBonusPerSlot[i] += attacker.gearRf[i] >= 8 ? 1 : 0;
+            }
+        }
+
+    } // for
+
+    // 砲撃戦.航空攻撃 雷装ボーナスの最小値を取得する
+    attacker.tpBonus = attacker.tpBonusPerSlot.reduce((v1, v2) => Math.min(v1, v2), Infinity);
+    if (attacker.tpBonus == Infinity) {
+        attacker.tpBonus = 0;
     }
-
-    // 369	https://wikiwiki.jp/kancolle/Swordfish%20Mk.III改(水上機型／熟練)
-    if (attacker.gearId[i] == GEAR_ID_MAP['Swordfish Mk.III改(水上機型/熟練)']) {
-      if (attacker.name == 'Gotoland andra') {
-        attacker.tpBonusPerSlot[i] = 3;
-        continue;
-      }
-    }
-
-    // 372	https://wikiwiki.jp/kancolle/天山一二型甲
-    if (attacker.gearId[i] == GEAR_ID_MAP['天山一二型甲']) {
-      if (attacker.ctype == '祥鳳型' && /改二/.test(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (['翔鶴型', '大鳳型'].includes(attacker.ctype)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (/龍鳳改/.test(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        attacker.tpBonusPerSlot[i] += /改二/.test(attacker.name) ? 1 : 0;
-        continue;
-      }
-    }
-
-    // 373	https://wikiwiki.jp/kancolle/天山一二型甲改(空六号電探改装備機)
-    if (attacker.gearId[i] == GEAR_ID_MAP['天山一二型甲改(空六号電探改装備機)']) {
-      if (attacker.ctype == '祥鳳型' && /改/.test(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (attacker.ctype == '龍鳳型') {
-        attacker.tpBonusPerSlot[i] = 1;
-        attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二' ? 1 : 0;
-        attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二戊' ? 2 : 0;
-        continue;
-      } else if (attacker.ctype == '千歳型' && /航改/.test(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (attacker.ctype == '飛鷹型') {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (['翔鶴型', '大鳳型'].includes(attacker.ctype)) {
-        attacker.tpBonusPerSlot[i] = 2;
-        continue;
-      } else if (['鈴谷航改二', '熊野航改二'].includes(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 2;
-        continue;
-      }
-    }
-
-    // 374	https://wikiwiki.jp/kancolle/天山一二型甲改(熟練／空六号電探改装備機)
-    if (attacker.gearId[i] == GEAR_ID_MAP['天山一二型甲改(熟練/空六号電探改装備機)']) {
-      if (attacker.ctype == '祥鳳型' && /改/.test(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (attacker.ctype == '龍鳳型') {
-        attacker.tpBonusPerSlot[i] = 1;
-        attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二' ? 1 : 0;
-        attacker.tpBonusPerSlot[i] += attacker.name == '龍鳳改二戊' ? 2 : 0;
-        continue;
-      } else if (attacker.ctype == '千歳型') {
-        attacker.tpBonusPerSlot[i] = 1;
-        continue;
-      } else if (attacker.ctype == '飛鷹型') {
-        attacker.tpBonusPerSlot[i] = 2;
-        continue;
-      } else if (['翔鶴型', '大鳳型'].includes(attacker.ctype)) {
-        attacker.tpBonusPerSlot[i] = 3;
-        continue;
-      } else if (['鈴谷航改二', '熊野航改二'].includes(attacker.name)) {
-        attacker.tpBonusPerSlot[i] = 2;
-        continue;
-      }
-    }
-
-    // 424	https://wikiwiki.jp/kancolle/Barracuda%20Mk.II
-    if (attacker.gearId[i] == GEAR_ID_MAP['Barracuda Mk.II']) {
-      if (attacker.country == 'イギリス' && ['軽空母', '正規空母', '装甲空母'].includes(attacker.stype)) {
-        attacker.tpBonusPerSlot[i] = 3;
-        continue;
-      }
-    }
-
-    // 425	https://wikiwiki.jp/kancolle/Barracuda%20Mk.III
-    if (attacker.gearId[i] == GEAR_ID_MAP['Barracuda Mk.III']) {
-      if (attacker.country == 'イギリス' && ['軽空母', '正規空母', '装甲空母'].includes(attacker.stype)) {
-        attacker.tpBonusPerSlot[i] = 1;
-        attacker.tpBonusPerSlot[i] += attacker.gearRf[i] >= 8 ? 1 : 0;
-      }
-    }
-
-  } // for
-
-  // 砲撃戦.航空攻撃 雷装ボーナスの最小値を取得する
-  attacker.tpBonus = attacker.tpBonusPerSlot.reduce((v1, v2) => Math.min(v1, v2), Infinity);
-  if (attacker.tpBonus == Infinity) {
-    attacker.tpBonus = 0;
-  }
 }
 
 /**
@@ -187,18 +187,18 @@ function getTpBonus(attacker) {
  */
 function tpBonus(attackerId, gearId, gearRf) {
 
-  // 処理する行サイズ
-  const ROW_SIZE = attackerId.reduce((v1, v2) => v1 + (v2 > 0 ? 1 : 0), 0);
+    // 処理する行サイズ
+    const ROW_SIZE = attackerId.reduce((v1, v2) => v1 + (v2 > 0 ? 1 : 0), 0);
 
-  // 攻撃艦
-  let attacker = new Array(ROW_SIZE);
+    // 攻撃艦
+    let attacker = new Array(ROW_SIZE);
 
-  // 行サイズぶんだけ砲撃戦.航空攻撃 雷装装備ボーナスを取得
-  for (let i = 0; i < ROW_SIZE; i++) {
-    attacker[i] = new TpAttacker(attackerId[i], gearId[i], gearRf[i]);
-    getTpBonus(attacker[i]);
-  }
+    // 行サイズぶんだけ砲撃戦.航空攻撃 雷装装備ボーナスを取得
+    for (let i = 0; i < ROW_SIZE; i++) {
+        attacker[i] = new TpAttacker(attackerId[i], gearId[i], gearRf[i]);
+        getTpBonus(attacker[i]);
+    }
 
-  // 砲撃戦.航空攻撃 雷装装備ボーナスを返す
-  return attacker.map(ship => ship.tpBonus);
+    // 砲撃戦.航空攻撃 雷装装備ボーナスを返す
+    return attacker.map(ship => ship.tpBonus);
 }
